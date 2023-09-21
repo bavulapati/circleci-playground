e7453297559e81b8773521b85db8cee4730377ed
Balakrishna Avulapati <bavulapati@gmail.com>
2023-09-21 12:43:23 +0530

    Use powershell profiles

---
+++ Powershell_profile.ps1
@@ -0,0 +1,2 @@
+Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
+Enter-VsDevShell -VsInstallPath 'C:\Program Files\Microsoft Visual Studio\2022\Community'
