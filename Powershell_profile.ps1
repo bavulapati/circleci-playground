e46ffb93fba1c83ffd122ccc207ece58a3c88651
Bala Avulapati <bavulapati@gmail.com>
2023-09-23 15:51:33 +0530

    use profile to refreshenv

--- Powershell_profile.ps1
+++ Powershell_profile.ps1
@@ -1,2 +1,4 @@
-Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
-Enter-VsDevShell -VsInstallPath 'C:\Program Files\Microsoft Visual Studio\2022\Community'
+# See https://learn.microsoft.com/en-us/visualstudio/ide/reference/command-prompt-powershell?view=vs-2022#developer-powershell
+& 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+refreshenv
