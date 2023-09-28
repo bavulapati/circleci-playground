1d6fa1273924a93c4a803ae79f594346335888ab
Bala Avulapati <bavulapati@gmail.com>
2023-09-28 15:11:13 +0530

    mix and match license, bundle

--- Powershell_profile.ps1
+++ Powershell_profile.ps1
@@ -1,4 +1,4 @@
-# See https://learn.microsoft.com/en-us/visualstudio/ide/reference/command-prompt-powershell?view=vs-2022#developer-powershell
-& 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 refreshenv
+# See https://learn.microsoft.com/en-us/visualstudio/ide/reference/command-prompt-powershell?view=vs-2022#developer-powershell
+& 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64
