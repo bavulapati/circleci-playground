a72989552996e77860791d58e59c5622859511b0
Bala Avulapati <bavulapati@gmail.com>
2023-11-23 15:43:26 +0530

    Remove powershell profile fiel to simplify

--- .circleci/powershell_profile.ps1
+++
@@ -1,2 +0,0 @@
-﻿# See https://learn.microsoft.com/en-us/visualstudio/ide/reference/command-prompt-powershell?view=vs-2022#developer-powershell
-& 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1' -Arch amd64 -HostArch amd64
